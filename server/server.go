80ab14b65209f830fb6ed6e25be1b3275b9f8ff6
Бобков Алексей Андреевич <11ap.abobkov@vas.arbitr.ru>
2020-03-27 16:38:21 +0400

    Попытки в изменения

--- server/server.go
+++ server/server.go
@@ -1,24 +1,31 @@
 package server
 
 import (
+	"net/http"
+	"realtimechat/structs"
+	"sync"
+
 	"github.com/go-chi/chi"
 	"github.com/gorilla/websocket"
-	"net/http"
-	"sync"
 )
 
+// Subscriber -
 type Subscriber func(msg string) error
 
+// Server -
 type Server struct {
-	router   *chi.Mux
-	upgrader *websocket.Upgrader
-
+	router      *chi.Mux
+	upgrader    *websocket.Upgrader
+	conf        *structs.Conf
 	submutex    *sync.Mutex
 	subscribers map[string]Subscriber
 }
 
+// New -
 func New() *Server {
 	router := chi.NewRouter()
+
+	conf := config()
 
 	upgrader := &websocket.Upgrader{
 		ReadBufferSize:  1024,
@@ -26,9 +33,9 @@ func New() *Server {
 	}
 
 	serv := &Server{
-		router:   router,
-		upgrader: upgrader,
-
+		router:      router,
+		upgrader:    upgrader,
+		conf:        conf,
 		submutex:    &sync.Mutex{},
 		subscribers: map[string]Subscriber{},
 	}
@@ -38,6 +45,7 @@ func New() *Server {
 	return serv
 }
 
+// Start -
 func (serv *Server) Start() error {
-	return http.ListenAndServe(":8085", serv.router)
+	return http.ListenAndServe(":"+serv.conf.Port, serv.router)
 }
